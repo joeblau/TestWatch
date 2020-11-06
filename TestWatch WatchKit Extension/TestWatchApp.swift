d35e3c91516169395bdc74f714bb78252a6a81b0
Joe Blau <im@joeblau.com>
2020-11-05 20:29:03 -0800

    Add session connection info

--- TestWatch WatchKit Extension/TestWatchApp.swift
+++ TestWatch WatchKit Extension/TestWatchApp.swift
@@ -6,14 +6,38 @@
 //
 
 import SwiftUI
+import WatchConnectivity
 
 @main
 struct TestWatchApp: App {
+    @WKExtensionDelegateAdaptor(ExtensionDelegate.self) var extensionDelegate
+
     var body: some Scene {
         WindowGroup {
             NavigationView {
                 ContentView()
             }
+        }
+    }
+    
+    class ExtensionDelegate: NSObject, WKExtensionDelegate, WCSessionDelegate {
+        var session: WCSession?
+
+        func applicationDidBecomeActive() {
+            if WCSession.isSupported() {
+                session = WCSession.default
+                session?.delegate = self
+                session?.activate()
+            }
+        }
+
+        func applicationWillResignActive() {
+        }
+
+        // MARK: - WCSessionDelegate
+
+        func session(_: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error _: Error?) {
+            print("Activatoin state: \(activationState.rawValue)")
         }
     }
 }
--- TestWatch/TestWatchApp.swift
+++ TestWatch/TestWatchApp.swift
@@ -6,12 +6,45 @@
 //
 
 import SwiftUI
+import WatchConnectivity
 
 @main
 struct TestWatchApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+    
+    class AppDelegate: NSObject, UIApplicationDelegate, WCSessionDelegate {
+        var session: WCSession?
+        
+        func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+            
+            if WCSession.isSupported() {
+                session = WCSession.default
+                session?.delegate = self
+                session?.activate()
+            }
+            
+            return true
+        }
+        
+        // MARK: - WCSessionDelegate
+
+        func session(_: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error _: Error?) {
+            print("Activatoin state: \(activationState.rawValue)")
+        }
+
+        func sessionDidBecomeInactive(_: WCSession) {
+            print("Session Inactive")
+        }
+
+        func sessionDidDeactivate(_: WCSession) {
+            print("Session Deactivate")
         }
     }
 }
